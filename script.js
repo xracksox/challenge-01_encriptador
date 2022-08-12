cae56a8a55ae3e5e3f684df04699737ef56f0731
OscarD <xracksox@hotmail.com>
2022-08-12 00:17:00 -0500

    Validación de texto

--- script.js
+++ script.js
@@ -7,28 +7,34 @@
 
 // Inicia Funcion para ENCRIPTAR Texto
 
-// Convertir texto a minusculas
-// identificar si hay simbolos
+// no mayusculas
+// no caracteres especiales 
+
 
 
 
 var encriptButton = document.querySelector("#encriptar");
 function encriptar() {
     var texto = document.querySelector("#textoCampo").value;
+    var validarTexto = /^[(a-z)+(0-9)+(\u00f1) ]*$/;
+    // alert(validarTexto.test(texto)); 
+
+if (validarTexto.test(texto)) {
     
     var textoAprocess = texto.split("");
-    
     for (let i = 0; i < textoAprocess.length; i++) {
-        textoAprocess[i]=textoAprocess[i].replace("e", "enter")
-        textoAprocess[i]=textoAprocess[i].replace("i", "imes")
-        textoAprocess[i]=textoAprocess[i].replace("a", "ai")
-        textoAprocess[i]=textoAprocess[i].replace("o", "ober")
-        textoAprocess[i]=textoAprocess[i].replace("u", "ufat")
+        textoAprocess[i] = textoAprocess[i].replace("e", "enter")
+        textoAprocess[i] = textoAprocess[i].replace("i", "imes")
+        textoAprocess[i] = textoAprocess[i].replace("a", "ai")
+        textoAprocess[i] = textoAprocess[i].replace("o", "ober")
+        textoAprocess[i] = textoAprocess[i].replace("u", "ufat")
     }
-    
     document.querySelector("#resultado").innerHTML = textoAprocess;
     var textoResultado = textoAprocess.join("");
     document.querySelector("#resultado").innerHTML = textoResultado;
+} else {
+    alert("No se permiten mayúsculas, acentos ni caracteres especiales")
+}    
 }
 
 encriptButton.onclick = encriptar;
@@ -43,8 +49,19 @@ var copiarTexto = document.querySelector("#copiar");
 function copiar() {
     var estomelocopias = document.querySelector("#resultado").value;
     navigator.clipboard.writeText(estomelocopias);
+    document.querySelector("#resultado").innerHTML = "";
+    alert("Texto copiado");
 }
 
 copiarTexto.onclick = copiar;
 
 // Termina Funcion para Copiar Texto
+
+// function desencriptar() {
+//     let textoEncriptado = textoIngresado.value.toLowerCase();
+//     let texto = textoEncriptado
+//         .replaceAll("enter", "e")
+//         .replaceAll("imes", "i")
+//         .replaceAll("ober", "o")
+//         .replaceAll("ai", "a")
+//         .replaceAll("ufat", "u");
