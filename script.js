082a5cf838ea2b9dcaea2114c515d69fbc5b1ba4
OscarD <xracksox@hotmail.com>
2022-08-12 00:50:37 -0500

    Lógica para desencriptar

--- script.js
+++ script.js
@@ -7,50 +7,58 @@
 
 // Inicia Funcion para ENCRIPTAR Texto
 
-// no mayusculas
-// no caracteres especiales 
-
-
-
-
 var encriptButton = document.querySelector("#encriptar");
 function encriptar() {
     var texto = document.querySelector("#textoCampo").value;
     var validarTexto = /^[(a-z)+(0-9)+(\u00f1) ]*$/;
     // alert(validarTexto.test(texto)); 
-
     if (validarTexto.test(texto)) {
-
         var textoEncriptado = texto
-        .replaceAll("e", "enter")
-        .replaceAll("i", "imes")
-        .replaceAll("a", "ai")
-        .replaceAll("o", "ober")
-        .replaceAll("u", "ufat")
-
+            .replaceAll("e", "enter")
+            .replaceAll("i", "imes")
+            .replaceAll("a", "ai")
+            .replaceAll("o", "ober")
+            .replaceAll("u", "ufat")
         document.querySelector("#resultado").innerHTML = textoEncriptado;
-
     } else {
         alert("No se permiten mayúsculas, acentos ni caracteres especiales")
     }
 }
-
 encriptButton.onclick = encriptar;
 
 // Termina Funcion para ENCRIPTAR Texto
 
 
+// Inicia Funcion para DESENCRIPTAR Texto
+
+var desencriptButton = document.querySelector("#desencriptar");
+function desencriptar() {
+    var texto = document.querySelector("#textoCampo").value;
+    var textoDesencriptado = texto
+        .replaceAll("enter", "e")
+        .replaceAll("imes", "i")
+        .replaceAll("ai", "a")
+        .replaceAll("ober", "o")
+        .replaceAll("ufat", "u")
+    document.querySelector("#resultado").innerHTML = textoDesencriptado;
+    document.querySelector("#textoCampo").value = "";
+}
+
+desencriptButton.onclick = desencriptar;
+
+// Termina Funcion para DESENCRIPTAR Texto
+
 
 // Inicia Funcion para Copiar Texto
 
-var copiarTexto = document.querySelector("#copiar");
+var btnCopiar = document.querySelector("#copiar");
 function copiar() {
-    var estomelocopias = document.querySelector("#resultado").value;
-    navigator.clipboard.writeText(estomelocopias);
+    var txtcopiar = document.querySelector("#resultado").value;
+    navigator.clipboard.writeText(txtcopiar);
     document.querySelector("#resultado").innerHTML = "";
     alert("Texto copiado");
+    document.querySelector("#textoCampo").value = "";
 }
-
-copiarTexto.onclick = copiar;
+btnCopiar.onclick = copiar;
 
 // Termina Funcion para Copiar Texto
